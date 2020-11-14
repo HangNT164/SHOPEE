8c019648d20dbbbc1056bebc938dae9f27c9ac23
HangNT164 <HangNT16499@gmail.com>
2020-11-14 12:38:31 +0700

    Add Fragment for card: HangNT

--- app/src/main/java/com/adapter/ISettingAccount.java
+++
@@ -1,7 +0,0 @@
-package com.adapter;
-
-import android.view.View;
-
-public interface ISettingAccount {
-    void onClick(View view, int position, boolean isLongClick);
-}
--- app/src/main/java/com/shopee/CartActivity.java
+++ app/src/main/java/com/shopee/CartActivity.java
@@ -1,8 +1,12 @@
 package com.shopee;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
+import com.fragment.CardEmptyFragment;
+import com.fragment.ListCardFragment;
 
 import static com.util.Helper.loadLocale;
 
@@ -13,6 +17,19 @@ public class CartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         loadLocale(getBaseContext(), "Language", "My_Lang");
         setContentView(R.layout.activity_cart);
+
+        // add fragment card
+        CardEmptyFragment cardEmptyFragment = new CardEmptyFragment();
+        ListCardFragment listCardFragment = new ListCardFragment();
+
+        // check user have card or not : use shared references
+        // empty
+        FragmentManager manager = getSupportFragmentManager();
+        manager.beginTransaction()
+                .replace(R.id.layoutCard, cardEmptyFragment, cardEmptyFragment.getTag()).commit();
+
+        // when have card will replace by first is listCard fragment
+
     }
 
     @Override
