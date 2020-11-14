f668a5673b8fc88a37ab4bf420026eb96b89f83b
HangNT164 <HangNT16499@gmail.com>
2020-11-14 13:33:44 +0700

    Fix Fragment for card: HangNT

--- app/src/main/java/com/fragment/CardEmptyFragment.java
+++ app/src/main/java/com/fragment/CardEmptyFragment.java
@@ -1,13 +1,15 @@
 package com.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import androidx.fragment.app.Fragment;
+
+import com.shopee.MainActivity;
 import com.shopee.R;
 
 /**
@@ -16,7 +18,7 @@ import com.shopee.R;
  * create an instance of this fragment.
  */
 public class CardEmptyFragment extends Fragment {
-
+    private Button button;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -61,6 +63,15 @@ public class CardEmptyFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_card_empty, container, false);
+        View view = inflater.inflate(R.layout.fragment_card_empty, container, false);
+        button = view.findViewById(R.id.buyNow);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 }
